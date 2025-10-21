3f23cf8086a65642633023b79740469a50ca52e9
Rsoria03 <rubensoria059@gmail.com>
2025-10-21 18:58:17 -0500

    Add files via upload

---
+++ Ejercicio1.js
@@ -0,0 +1,6 @@
+function ContarCaracteres() {
+    let frase, cl, longf;
+    frase = prompt("Ingrese una frase: ");
+    longf = frase.length;
+    alert (`${frase} \nLa frase tiene ${longf} caracteres`);
+}ContarCaracteres();
