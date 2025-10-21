bb0c1390f1d5daf41b0c846b8894e8aa2084e796
Rsoria03 <rubensoria059@gmail.com>
2025-10-21 18:58:52 -0500

    Ejercicio1.js

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -1,6 +1,7 @@
 function ContarCaracteres() {
-    let frase, cl, longf;
+    let frase, longf;
     frase = prompt("Ingrese una frase: ");
     longf = frase.length;
     alert (`${frase} \nLa frase tiene ${longf} caracteres`);
+
 }ContarCaracteres();
