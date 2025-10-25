711ec15abc4b0e4afc0489c20d2ebe395c74bbd0
Rsoria03 <rubensoria059@gmail.com>
2025-10-25 11:31:05 -0500

    Delete Ejercicio1.js

--- Ejercicio1.js
+++
@@ -1,7 +0,0 @@
-function ContarCaracteres() {
-    let frase, longf;
-    frase = prompt("Ingrese una frase: ");
-    longf = frase.length;
-    alert (`${frase} \nLa frase tiene ${longf} caracteres`);
-
-}ContarCaracteres();
